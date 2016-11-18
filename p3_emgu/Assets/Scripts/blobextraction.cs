2b2536e5e609caa297c3060fc269bd148b35e2ff
Ronny Vangsted <ronny@h219.aau1x.klient3.nordkraft.site.aau.dk>
2016-11-18 11:36:38 +0100

    Blob extraction - DLL error

---
+++ p3_emgu/Assets/Scripts/blobextraction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+public class blobextraction : MonoBehaviour {
+
+	public RawImage rawimage;
+	public static string imagePath() {
+		return string.Format("{0}/Resources/photo.png", 
+			Application.dataPath);
+	}
+
+	public Texture2D returnAsTexture (Image<Gray, byte> imgInput) {
+		Texture2D img = new Texture2D(imgInput.Height, imgInput.Width);
+		for (int y = 0; y < img.height - 1; y++) {
+			for (int x = 0; x < img.width - 1; x++) {
+				UnityEngine.Color color = new UnityEngine.Color((int)imgInput[x,y].Intensity, (int)imgInput[x,y].Intensity, (int)imgInput[x,y].Intensity);
+				img.SetPixel(x, y, color);
+			}
+		}
+
+		img.Apply();
+		return img;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		string imgPath = imagePath ();
+		Mat srcImage = new Mat(imgPath, 0);
+
+		Image<Gray, byte> blobImage = srcImage.ToImage<Gray, byte>();
+
+		rawimage.texture = returnAsTexture(blobImage);
+		rawimage.material.mainTexture = returnAsTexture(blobImage);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
